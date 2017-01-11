7fe7645716f0252c8ae5ed9d7b68eec64a67484d
Eva Lieu <itsevalieu@gmail.com>
2017-01-10 18:35:58 -0800

    Data outlining for the results

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+//Data
+//==============================================
+var data = [];
+
+var results = {
+	name: $("#name").val(),
+	photo: $("#imageLink").val().trim(),
+	results: [
+		$("#question1"), 
+		$("#question2"), 
+		$("#question3")
+		$("#question4"),
+		$("#question5"),
+		$("#question6"),
+		$("#question7"),
+		$("#question8"),
+		$("#question9"),
+		$("#question10")
+	]
+}
--- server.js
+++ server.js
@@ -17,8 +17,35 @@ app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
 
+//Data
+//============================================================
+var surveyResults = [
+{
+  name:"Ahmed",
+  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+	]
+}
+];
+
 //Routes
 //============================================================
 app.get("/", function(request, response){
 	response.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+app.get("/survey", function(request, response){
+	response.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+app.get("/api-friends", function(request, response){
+	response.json(surveyResults);
 });
