452f30d4e9a7419af6244d88d026a1b8f66c6198
Eva Lieu <itsevalieu@gmail.com>
2017-01-26 01:34:21 -0800

    Completed functions, made most changes to api-routes.js; modal still needs work, but overall app is functioning

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,18 @@
 //Data
 //============================================================
-var surveyResults = [
-	{
-	  name:"Ahmed",
-	  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-	  scores:[
-	     5,
-	     1,
-	     4,
-	     4,
-	     5,
-	     1,
-	     2,
-	     5,
-	     4,
-	     1
-		]
-	}
-];
+var friends = [{
+	name:"Ahmed",
+	photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+}, {
+	name:"Cindy",
+	photo:"http://www.dannyst.com/blogimg/gallery-portraits-of-strangers-18.jpg",
+	scores:[2, 1, 3, 3, 5, 1, 2, 4, 3, 3]
+}, {
+	name:"Ben",
+	photo:"http://www.drodd.com/images15/man5.jpg",
+	scores:[4, 1, 4, 2, 4, 1, 1, 4, 4, 3]
+}];
 
 //Export object for accessibility to other files
-module.exports = surveyResults;
+module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,13 +4,77 @@ var path = require("path");
 
 //Data
 //============================================================
-var surveyResults = require("../data/friends.js");
+var friends = require("../data/friends.js");
 
 //Routes
 //============================================================
 
 module.exports = function(app){
 	app.get("/api-friends", function(request, response){
-		response.json(surveyResults);
+		console.log(friends);
+		response.json(friends);
 	});
+
+	app.post("/api-friends", function(request, response){
+		//takes in JSON input of new friend
+		var newFriend = request.body;
+
+		console.log(newFriend);
+		
+		var nFriendScores = [];
+		var friendsScores = [];
+		var totalScores = [];
+
+		//converts string to int
+		nFriendScores = changeToIntegers(newFriend.scores);
+		console.log("New friend scores: " + nFriendScores);
+		
+		//compare nfriendscore with rest of friends in array & get total scores
+		for(var i = 0; i < friends.length; i++) {
+			friendsScores[i] = changeToIntegers(friends[i].scores);
+			totalScores[i] = calculateTotal(friendsScores[i], nFriendScores);
+		}
+		var bestMatch =  friends[calculateLeastDiff(totalScores)];
+		//return to survey.html data
+		console.log("Best match is " + bestMatch.name);
+
+		response.json(bestMatch);
+
+
+		//add to friends array (must add after so best match isn't new friend)
+		friends.push(newFriend);
+	});
+
+	//convert string scores to integers
+	function changeToIntegers(array){
+		var scores = [];
+		for(var i = 0; i < 10; i++){	
+			scores[i] = parseInt(array[i]);	
+		}
+		return scores;
+	}
+
+	//get the total after calculating differences in each score
+	function calculateTotal(arr1, arr2){
+		var difference = [];
+		var total = 0;
+
+		for(var i = 0; i < 10; i++){
+			difference[i] = Math.abs(arr1[i] - arr2[i]);
+			total += difference[i];
+		}
+		return total;
+	}
+
+	function calculateLeastDiff(array){
+		var min = array[0];
+		var minIndex = 0;
+		for(var i = 0; i < array.length; i++){
+			if(array[i] < min){
+				minIndex = i;
+				min = array[i];
+			}
+		}
+		return minIndex;
+	}
 };
